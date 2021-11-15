c46aae9cbac33e1b0086a5041dc63cb43b279cbc
Vergiananta <christian.vergi@gmail.com>
2021-11-15 13:48:29 +0700

    [3-COMPONENT] ketika terdapat >2 komponen

--- src/component/counter-product/counter_product.jsx
+++ src/component/counter-product/counter_product.jsx
@@ -1,36 +1,8 @@
 import { Component } from "react";
 import { Button, Card, CardBody, CardFooter, CardHeader, Col } from "reactstrap";
+import CounterButton from "./counter_button";
 
 export default class Counter extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            order: 4
-        }
-    }
-
-    handleCounterChange = (value) => {
-        this.props.onCounterChange(value);
-    }
-
-    handlePlus = () => {
-        this.setState({
-            order: this.state.order + 1
-        }, () => {
-            this.handleCounterChange(this.state.order);
-        })
-    }
-
-    handleMinus = () => {
-        if (this.state.order > 0) {
-            this.setState({
-                order: this.state.order - 1
-            }, () => {
-                this.handleCounterChange(this.state.order);
-            })
-        }
-    }
 
     render() {
         return (
@@ -39,13 +11,8 @@ export default class Counter extends Component {
                     <CardHeader tag="strong">
                         <p>Counter</p>
                     </CardHeader>
-                    <CardBody>
-                        <h5 className="text-center">{this.state.order}</h5>
-                    </CardBody>
-                    <CardFooter>
-                        <Button type="button" color="primary" className="float-left" onClick={this.handlePlus}>+</Button>
-                        <Button type="button" color="primary" className="float-right" onClick={this.handleMinus}>-</Button>
-                    </CardFooter>
+                    {/* ditambahkan properti oncounterChange lagi agar data dapat diakses */}
+                    <CounterButton onCounterChange={(value) => this.props.onCounterChange(value)}/>
                 </Card>
             </Col>
         )
